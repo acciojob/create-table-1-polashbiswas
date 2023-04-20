5eaec79c058ce0908cf87ed87ca3220d7e887777
polashbiswas <30291056+polashbiswas@users.noreply.github.com>
2023-04-20 15:31:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  const table = document.getElementById("sampleTable");
+	let tr = document.createElement("tr");
+	let td1 = document.createElement("td");
+	let td2 = document.createElement("td");
+    td1.innerHTML = "New Cell1";
+    td2.innerHTML = "New Cell2";
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+	table.insertBefore(tr, table.firstChild);
 }
